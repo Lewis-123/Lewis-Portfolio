b216f774fcd907b73e91c1d0be63c69c22c9a67d
Lewis-123 <mucherulewis@gmail.com>
2026-09-10 18:15:04 +0300

    Add custom error page and portfolio branding

--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,80 @@ const express = require("express");
 const router = express.Router();
 
 
-// Home
+// Home Page
+
 router.get("/", (req, res)=>{
+
     res.render("index", {
+
         title:"Home"
+
     });
+
 });
 
 
-// About
+
+
+// About Page
+
 router.get("/about",(req,res)=>{
+
     res.render("about",{
+
         title:"About Me"
+
     });
+
 });
 
 
-// Projects
+
+
+// Projects Page
+
 router.get("/projects",(req,res)=>{
+
     res.render("projects",{
+
         title:"Projects"
+
     });
+
 });
 
 
-// Contact
+
+
+// Contact Page
+
 router.get("/contact",(req,res)=>{
+
     res.render("contact",{
+
         title:"Contact"
+
     });
+
 });
+
+
+
+
+
+// 404 Page
+
+router.use((req,res)=>{
+
+    res.status(404).render("404",{
+
+        title:"Page Not Found"
+
+    });
+
+});
+
+
 
 
 module.exports = router;
